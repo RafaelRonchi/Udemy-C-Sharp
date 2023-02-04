38430f09e2aa41def8a8d45ccaa98246c07ad4c6
RafaelRonchi <rafael.e01@aluno.ifsc.edu.br>
2023-02-04 03:12:33 -0300

    Solution POO Console

---
+++ ConsolePOO/POO/Propriedades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+            t.Nome = "Rafael";
+            t.Idade = 90;
+
+            Console.WriteLine(t.Nome + " " + t.Sobrenome);
+            Console.WriteLine(t.Idade);
+
+            Console.ReadKey();
+        }
+    }
+}
