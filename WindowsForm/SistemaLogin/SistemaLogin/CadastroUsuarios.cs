82e7af8ef96ad3c32d5368d89686457478e52d1a
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-02-13 19:50:38 -0300

    Arquivos Sistema de login com windows forms

---
+++ WindowsForm/SistemaLogin/SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    static class CadastroUsuarios
+    {
+        private static Usuario[] usuarios = 
+        { 
+            new Usuario(){ Nome = "Rafael", Senha="123"},
+            new Usuario(){ Nome = "admin", Senha= "admin"}
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; } 
+            private set { _userLogado = value; }
+        }
+
+        public static bool Login(string nome, string senha)
+        {
+            foreach (Usuario u in usuarios)
+            {
+                if(u.Nome == nome && u.Senha == senha)
+                {
+                    UsuarioLogado = u;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
